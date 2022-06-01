d7e5ed3f6681af392584bc6e93567fafae36726b
AndreCosta <andre.costa101@gmail.com>
2022-06-01 19:58:54 -0300

    Estrutura do Hook de Autenticação

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
+import { AuthProvider } from './src/hooks/auth';
 
 import theme from './src/theme';
 
@@ -22,7 +23,9 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style='light' translucent backgroundColor='transparent'/>
-      <SignIn />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
     </ThemeProvider>
   );
 }
